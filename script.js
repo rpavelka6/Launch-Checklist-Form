eed4494cde596cd6069d5391b11cf48052ef23e3
Rebecca Pavelka <pavelka6@sbcglobal.net>
2019-10-07 19:45:59 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Write your JavaScript code here!
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
+/* This block of code here shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
    <li>Name: ${}</li>
